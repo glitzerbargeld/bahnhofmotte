a92a55a503cf0e9657325b5add54d521ca04b58b
Torben Jäckel <torben.jaeckel@gmail.com>
2020-10-23 09:46:31 +0200

    blur effect

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,7 @@
 
 /*Navbar Show/Hide*/
 const menu = document.querySelector('.menu');
-const menupoints = document.querySelectorAll('.nav-point')
-
+const menupoints = document.querySelectorAll('.nav-point');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle("change");
@@ -17,5 +16,45 @@ menupoints.forEach(function(elem) {
     });
 });
 
-/*Scroll Function*/ 
+/*Scroll Function*/
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
 
